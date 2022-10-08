fa4a06269eb4d518bb2f0ab5219d43cd0089ee7a
Legion-1315 <105158761+Legion-1315@users.noreply.github.com>
2022-10-08 15:35:35 +0530

    second commit

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        let { title, description, urlToImage, url, date, author, source} = this.props;// destructuring in js
+    return (
+      <div className='my-3'>
+        <div className="card">
+        <img src={urlToImage} className="card-img-top" alt="..."/>
+        <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">Last updated {new Date(date).toGMTString()} by {author}</small></p>
+            <p className='card-text'><strong>Source : {source}</strong></p>
+            <a href={url} target='_blank' rel="noreferrer" className="btn btn-sm btn-dark">READ MORE</a>
+        </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
